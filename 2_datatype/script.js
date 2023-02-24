d40d65ea4ef8fa74f7cad323d728349542766dee
Rupesh Singh <S220630769@itsbhm.org>
2023-02-24 16:59:11 +0530

    Commit #5255

---
+++ 2_datatype/script.js
@@ -0,0 +1,16 @@
+//1) Number (6745878)
+//2) String ("", '' , Alphabatically Data)
+//3) Boolean (True or False Statement)
+//4) undefined (Not Defined Variable)
+//5) null  (Null)
+//6) Symbol  (For holding Small Number of data)
+//7)BigInt  (For holding Biggest Numbers)
+
+var x = "This is data type";
+let year;
+console.log(typeof year);
+
+
+// const, let  and var 
+
+
